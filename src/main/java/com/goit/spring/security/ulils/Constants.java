412b0223e81ef52efc2b2ebac6d08fca37b36a86
Yuriy Tsarenko <tsarenkoyuriidev@gmail.com>
2023-07-13 15:42:07 +0300

    fixup tests

--- src/main/java/com/goit/spring/security/ulils/Constants.java
+++ src/main/java/com/goit/spring/security/ulils/Constants.java
@@ -2,6 +2,6 @@ package com.goit.spring.security.ulils;
 
 public class Constants {
     //============================== PROPERTY KEYS =======================
-    public static final String ADMIN_USERNAME = "gcp.env.admin";
-    public static final String ADMIN_PASSWORD = "gcp.env.password";
+    public static final String ADMIN_USERNAME = "gcp.vars.admin.user";
+    public static final String ADMIN_PASSWORD = "gcp.vars.admin.password";
 }
